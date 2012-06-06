149a493d96c15e606ef248440ea11e734be34ebc
Mirko Caserta <mirko.caserta@gmail.com>
2012-06-06 18:08:42 +0200

    Cosmetics

--- example-03/api/src/main/java/com/nexse/swat/tutorial/mvn/example03/BookStore.java
+++ example-03/api/src/main/java/com/nexse/swat/tutorial/mvn/example03/BookStore.java
@@ -4,18 +4,19 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import java.util.Set;
 
 @Path("/bookstore")
 public interface BookStore {
 
     @GET
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     Set<Book> getBooks();
 
     @GET
     @Path("{id}")
-    @Produces("application/json")
+    @Produces(MediaType.APPLICATION_JSON)
     Book getBook(@PathParam("id") long id);
 
 }
